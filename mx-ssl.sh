27de9bc2a6bc87edecd65cf21c3196b08c3b635e
Uwe Sommer <uwe@usommer.de>
2018-02-17 17:05:29 +0100

    Update mx-ssl.sh

--- mx-ssl.sh
+++ mx-ssl.sh
@@ -4,29 +4,33 @@
 ## uwe@usommer.de
 ## 01/2018
 
-## todo:
-## more error checks
-## display complete TLS analysis
-
+function mx_ssl()
+{
+## catch no parameter error
 if [ "$#" -le "0" ]; then
-	echo "domain-name required"
-		else
-			domain="$1"
-			cur_date=$(date +%s)														## convert current date to seconds (Epoch time)
-			mx=$(dig +short mx "$domain" | awk '{print $2}')							## get mx records and read hosts
-			echo "remaining days for SSL certificates on Mailservers"
-			echo ""
-			echo "Domain: $domain"
-			echo ""
-			printf "Servers: \n$mx"
-			echo ""
-			echo ""
-			echo "Days remaining for certificates:"
-			echo ""
-			for server in $mx
-				do
-					date2=$(echo quit |openssl s_client -starttls smtp -connect "$server":25 2>/dev/null | openssl x509 -noout -enddate |cut -d"=" -f2)			## get SSL expiry date from TLS connection
-					exp_date_smtp=$(date -j -f "%b %d %T %Y %Z" "$date2" +"%s")		## convert expiry date timestamp to seconds
-					echo "scale=0; ($exp_date_smtp - $cur_date) / 86400" |bc -l			## calculate expiry time and convert result to days
-			done
+    echo "domain-name required"
+    return
 fi
+domain="$1"
+## convert current date to seconds (Epoch time)
+cur_date=$(date +%s)
+## get mx server records from DNS
+mx=$(dig mx "$domain" |awk '$4=="MX" {print $6}')
+## get mx records and read hosts
+printf "%s\n" "remaining days for SSL certificates on Mailservers" "" "Domain: $domain" "" "Servers:" "$mx" "" "" "Days remaining for certificates:" ""
+## loop through all servers. This does not work on zsh shell because of worsplitting bug
+## you need to set "setopt shwordsplit" in your .zshrc
+for server in $mx
+do
+    ## get certificate end date from port connection via openssl
+    date2=$(echo quit |openssl s_client -starttls smtp -connect "$server":25 2>/dev/null | openssl x509 -noout -enddate 2>null |cut -d"=" -f2)
+    ## convert enddate to seconds. OSX uses special date conversion.
+    if [[ $OSTYPE == darwin* ]]; then
+    exp_date_smtp=$(LANG=C date -j -f "%b %d %T %Y %Z" "$date2" +"%s" 2>/dev/null)
+    else
+    exp_date_smtp=$(date -d "$date2" +"%s")
+    fi
+    ## calculate days remaining
+    echo "scale=0; ($exp_date_smtp - $cur_date) / 86400" |bc -l 
+done
+}
